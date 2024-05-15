a14205cc3fc9c5312f665b3a0d62b6f612861b32
Naeema bargir <bargirn5@gmail.com>
2024-05-15 18:32:29 +0530

    header, cookies updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import CookieCard from "./layout/cookieCard";
 import Footer from "./layout/footer";
 import Header from "./layout/header";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <CookieCard />
         <Footer />
       </body>
     </html>
---
+++ src/app/layout/index.tsx
@@ -0,0 +1,5 @@
+import Header from "./header";
+import Footer from "./footer";
+import CookieCard from "./cookieCard";
+
+export { Header, Footer, CookieCard }
