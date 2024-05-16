a292276ba36313be5ff3fb73d4d6ab7e8c9f7e14
Shahid shah <114140444+Shahid7424@users.noreply.github.com>
2024-05-16 16:21:26 +0530

    add search page

---
+++ src/app/policy/page.tsx
@@ -0,0 +1,202 @@
+import React from "react";
+
+const Policy = () => {
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        <h2 className="col text-center font-bold mb-4">Privacy Policy</h2>
+      </div>
+
+      <div className="responBx">
+        <h6 className="sub-heading ">
+          This Privacy Policy was last updated on 12th May 2024.
+        </h6>
+        <p>
+          ReservationKart Online Limited, including all legal entities under the
+          ReservationKart group (hereafter referred to as "ReservationKart" or
+          "we") with an exception of ReservationKart Online Freight Services
+          Private Limited (YOFSPLL), respects your (customers - hereafter
+          referred as "you" or "user") privacy and values our relationship with
+          our business partners, clients, third parties having contractual
+          relationship with ReservationKart (hereafter referred to as "third
+          party" or "service provider"), and any party who purchases/intend to
+          purchase/inquire about any product(s) and/or service(s) made available
+          by ReservationKart through any of ReservationKart's customer interface
+          channels including Website, mobile site, mobile app & offline channels
+          including call centers and offices (collectively referred herein as
+          "Sales Channels"). We are committed to protecting your personally
+          identifiable information (hereafter referred as "information" or
+          "personal information" or "personal data" or "special category of
+          personal data") by handling it responsibly and safeguarding it using
+          appropriate technical and organizational measures.
+        </p>
+        <p>
+          We want to assure you that we follow appropriate standards when it
+          comes to protecting your privacy on our Sales Channel. This policy
+          outlines the types of personal information collected on our Website,
+          how this data is used/processed and safeguarded as well as how users
+          may exercise their rights with respect to personal information.
+        </p>
+        <p>
+          This Privacy Policy applies to ReservationKart.com, ReservationKart
+          company pages, communications and services ("Services"), including
+          off-site Services, such as our email services, customer care and
+          support services and the "Contact us"; "Apply with ReservationKart"
+          and "Share on" plugins on our digital properties.
+        </p>
+        <p>
+          By using or accessing the Website or other Sales Channels, the user
+          hereby agrees with the terms of this Privacy Policy and the contents
+          herein.
+        </p>
+        <p>
+          This Privacy Policy does not apply to any website(s), mobile sites and
+          mobile apps of third parties, even if their websites/products are
+          linked to our Website. User should take note that the information and
+          privacy practices of ReservationKart's business partners, advertisers,
+          sponsors or other sites to which we provide hyperlink(s), may be
+          materially different from this Privacy Policy. Accordingly, it is
+          recommended that you review the privacy statements and policies of any
+          such third parties.
+        </p>
+        <p>
+          <b>ReservationKart for Business (Corporate):</b> In the event
+          ReservationKart is providing services to you as part of our agreement
+          with your employer, contractor, business partner, affiliate or any
+          other party who is having a contractual arrangement with
+          ReservationKart, then the terms and conditions stipulated in such
+          contractual arrangement shall prevail over this data privacy policy.
+          Such agreement may involve sharing personal data they control as part
+          of that engagement. Our services may also include processing personal
+          data under our client's control on our hosted software applications.
+          All such processing will be governed by ReservationKart's client
+          privacy policy.
+        </p>
+        <p>
+          In case you book travel services on behalf of your family or a minor,
+          you agree:
+        </p>
+        <ul className="ml-4">
+          <li>
+            To inform the individual about the content of this privacy policy
+          </li>
+          <li>
+            To obtain any legally-required consent, for the collection, use,
+            disclosure, and transfer (including cross-border transfer) of
+            individual's personal information, from the individual in accordance
+            with the legal and regulatory requirements
+          </li>
+        </ul>
+        <p>
+          In such a scenario, ReservationKart will be acting as data processors
+          while your respective employer, contractor, business partner,
+          affiliate shall be acting as the data controller. It will be the
+          obligation of the data controller to seek consent of the end customer.
+        </p>
+        <p>
+          <b>1. What Personal Data shall we collect?</b>
+        </p>
+        <p>
+          a. We collect the following personal and sensitive information from
+          you when you transact through us:
+        </p>
+        <ul className="ml-4">
+          <li>Name, gender, age and date of birth</li>
+          <li>
+            Contact information such as address, email, and mobile phone number
+          </li>
+          <li>Country of residence</li>
+          <li>
+            Payment Details like Credit Card/ Debit Card details along with
+            their expiry date (billing address in case of international
+            debit/credit card).
+          </li>
+          <li>Passport Number & frequent flyer number (wherever needed)</li>
+          <li>
+            Lifestyle and social circumstances (for example, your hobbies)
+          </li>
+          <li>
+            Family details/information (for example, your marital status and
+            dependents)
+          </li>
+          <li>
+            Employment and education details (for example, the organization you
+            work for, your job title and your education details)
+          </li>
+          <li>
+            Financial and tax-related information (like your income, investments
+            and tax residency)
+          </li>
+          <li>
+            Postings or messages on any blogs, forums, platforms, wikis or
+            social media applications of ReservationKart and ReservationKart's
+            third parties
+          </li>
+          <li>IP address, browser type and language, your access times</li>
+          <li>Information in any complaints you make</li>
+          <li>Details of how you use our products and services</li>
+          <li>
+            CCTV footage and other information we collect when you access our
+            premises
+          </li>
+          <li>
+            Recording of telephone calls to our representatives and call centers
+          </li>
+          <li>
+            Details of how you like to interact with us (through email, SMS or
+            WhatsApp) and other similar information relevant to our relationship
+          </li>
+          <li>Details shared by you over our chatbot - Yuva</li>
+        </ul>
+        <p>
+          b. The personal data we collect may also include 'sensitive' or
+          'special categories' of personal data such as details about your:
+        </p>
+        <ul className="ml-4">
+          <li>
+            Dietary requirements (for example, while providing your meal
+            preference during flight/ hotel bookings)
+          </li>
+          <li>
+            Health (so that we can make it easy for you to access our buildings,
+            products and services, and/or any special assistance required by
+            you)
+          </li>
+          <li>
+            Sexual orientation (if you provide us with details of your spouse or
+            partner)
+          </li>
+        </ul>
+        <p>
+          If you choose not to provide, or object to us processing the
+          information we collect, we may not be able to process your
+          instructions or continue to provide some or all of our services to you
+          (including our clients)Personal information of other
+          customers/users/travelers
+        </p>
+        <p>
+          If you provide personal information to us regarding individuals other
+          than yourself, you agree
+        </p>
+        <ul className="ml-4">
+          <li>
+            To inform the individual about the content of this privacy policy
+          </li>
+          <li>
+            To obtain any legally-required consent, for the collection, use,
+            disclosure, and transfer (including cross-border transfer) of
+            individual's personal information, from the individual in accordance
+            with the legal and regulatory requirements
+          </li>
+        </ul>
+        <p>c. Personal information of child</p>
+        <p>
+          The Services are not intended for users under the age of 16
+          (Permissible Age), unless{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Policy;
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client";
+import React from "react";
+
+import { RouteCard } from "../component";
+import { routesData } from "../utils/data";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="container mt-4">
+      
+      {/* search cards */}
+      {routesData.map((routes, index) => (
+        <RouteCard routes={routes} key={index} />
+      ))}
+    </div>
+  );
+};
 
-export default Search
+export default Search;
