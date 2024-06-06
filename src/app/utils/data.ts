c2a9f43d141f104363423b23f47e0c0d0ac15837
Shahid shah <114140444+Shahid7424@users.noreply.github.com>
2024-06-06 18:51:42 +0530

    add data

--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -66,18 +66,18 @@ export const recommendedCardData = [
 ];
 
 export const destinationData = [
-  { imgSrc: destination01, destination: "Hawai", hotels: 12683 },
-  { imgSrc: destination02, destination: "Turkey", hotels: 12683 },
-  { imgSrc: destination03, destination: "Paris", hotels: 12683 },
-  { imgSrc: destination04, destination: "Maldives", hotels: 12683 },
-  { imgSrc: destination05, destination: "Australia", hotels: 12683 },
-  { imgSrc: destination06, destination: "Rome", hotels: 12683 },
-  { imgSrc: destination07, destination: "England", hotels: 12683 },
-  { imgSrc: destination08, destination: "London", hotels: 12683 },
-  { imgSrc: destination09, destination: "New Zealand", hotels: 12683 },
-  { imgSrc: destination10, destination: "Peru", hotels: 12683 },
-  { imgSrc: destination11, destination: "France", hotels: 12683 },
-  { imgSrc: destination12, destination: "Paris", hotels: 12683 },
+  { imgSrc: destination01, destination: "Hawai", hotels: 13585 },
+  { imgSrc: destination02, destination: "Turkey", hotels: 14687 },
+  { imgSrc: destination03, destination: "USA", hotels: 11483 },
+  { imgSrc: destination04, destination: "Maldives", hotels: 13733 },
+  { imgSrc: destination05, destination: "Australia", hotels: 15883 },
+  { imgSrc: destination06, destination: "Rome", hotels: 16683 },
+  { imgSrc: destination07, destination: "England", hotels: 19583 },
+  { imgSrc: destination08, destination: "London", hotels: 12483 },
+  { imgSrc: destination09, destination: "New Zealand", hotels: 14927 },
+  { imgSrc: destination10, destination: "Peru", hotels: 14383 },
+  { imgSrc: destination11, destination: "France", hotels: 17383},
+  { imgSrc: destination12, destination: "Paris", hotels: 15884 },
 ];
 
 export const routesData = [
